4840c36e646f53d82036d2db2d5a5220c9d6f309
Thomas Zoratto <thomas.zoratto@gmail.com>
2016-07-26 12:52:30 +0200

    added a missing comment

--- utils/validationErrors.js
+++ utils/validationErrors.js
@@ -2,6 +2,12 @@
 
 const mongoose = require('mongoose');
 
+/**
+ * Checks if the given error is a validation error.
+ *
+ * @param err
+ * @return {*} - The object containing the validation errors per field.
+ */
 module.exports = function (err) {
     var validationErrors = null;
     if (err && err instanceof mongoose.Error.ValidationError) {
